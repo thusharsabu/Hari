4e4bccb7af162379c4ddb1b5ec9a8d4a8f3bb361
thushar <thushar.rey@gmail.com>
2020-05-19 22:33:57 -0500

    Updates

---
+++ precache-manifest.a24324a73245f235c046d5188d6f5976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "473ee4e7975299964591",
+    "url": "/hari/static/js/runtime~main.473ee4e7.js"
+  },
+  {
+    "revision": "746eb3e9558aec7e972e",
+    "url": "/hari/static/js/main.746eb3e9.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/hari/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "746eb3e9558aec7e972e",
+    "url": "/hari/static/css/main.c4c4df8b.chunk.css"
+  },
+  {
+    "revision": "f458086074295210f96ab1d63184ed1a",
+    "url": "/hari/index.html"
+  }
+];
